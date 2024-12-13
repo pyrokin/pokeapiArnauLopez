cb02d065a68629d134f5f1927e2ced42db5f3c7d
Arnau-Lopez <154209033+pyrokin@users.noreply.github.com>
2024-12-13 09:24:30 +0100

    Updates

--- js/script.js
+++ js/script.js
@@ -18,7 +18,18 @@ const typeChart = {
     fire: { fire: 0.5, water: 0.5, grass: 2, ice: 2, bug: 2, rock: 0.5, dragon: 0.5, steel: 2 },
     water: { fire: 2, water: 0.5, grass: 0.5, ground: 2, rock: 2, dragon: 0.5 },
     grass: { fire: 0.5, water: 2, grass: 0.5, poison: 0.5, ground: 2, flying: 0.5, bug: 0.5, rock: 2, dragon: 0.5, steel: 0.5 },
-    // Add other types here...
+    electric: { water: 2, electric: 0.5, grass: 0.5, ground: 0, flying: 2, dragon: 0.5 },
+    ice: { fire: 0.5, water: 0.5, grass: 2, ice: 0.5, ground: 2, flying: 2, dragon: 2, steel: 0.5 },
+    fighting: { normal: 2, ice: 2, rock: 0.5, dark: 0.5, steel: 0.5 },
+    poison: { grass: 2, poison: 0.5, ground: 0.5, psychic: 2, bug: 0.5, rock: 0.5, ghost: 0.5, dark: 0.5 },
+    ground: { fire: 2, electric: 2, grass: 0.5, poison: 0.5, flying: 0.5, bug: 0.5, rock: 2, steel: 2 },
+    flying: { grass: 0.5, fighting: 2, bug: 2, rock: 0.5, steel: 0.5 },
+    psychic: { fighting: 2, poison: 2, psychic: 0.5, dark: 0 },
+    bug: { fire: 0.5, grass: 2, fighting: 0.5, poison: 0.5, flying: 0.5, psychic: 2, ghost: 0.5, dark: 0.5, steel: 0.5 },
+    rock: { fire: 2, ice: 2, fighting: 0.5, ground: 0.5, flying: 0.5, bug: 2, steel: 0.5 },
+    ghost: { normal: 0, poison: 0.5, bug: 0.5, ghost: 2, dark: 2 },
+    dragon: { dragon: 2, steel: 0.5 },
+    dark: { fighting: 0.5, psychic: 0.5, ghost: 2, dark: 0.5 },
     default: {}
 };
 
